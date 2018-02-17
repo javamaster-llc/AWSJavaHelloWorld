121cda6dda025dd34244bf8a51448218f71b01a7
Jing Tan <jingyoko@gmail.com>
2018-02-17 10:43:44 -0500

    added more files

--- AWSJavaHelloWorld/src/main/java/com/ca/AWSJavaHelloWorld/Credentials.java
+++ AWSJavaHelloWorld/src/main/java/com/ca/AWSJavaHelloWorld/Credentials.java
@@ -1,5 +1,14 @@
 package com.ca.AWSJavaHelloWorld;
 
+
+/**
+ * Created by @author tanji11
+ */
+
 public class Credentials {
+	
+    public static final String access_key_id= "AKIAJHSOFR2CTVXNGVCA";
+
+    public static final String secret_access_key ="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";
 
 }
